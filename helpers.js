b6fb367210a89002f6d35fd92e7f0be754db8c55
Caroline Moore <calobee@gmail.com>
2025-04-06 13:10:01 -0400

    Add styles

--- helpers.js
+++ helpers.js
@@ -18,7 +18,13 @@ function createMessage( content, showClose = true ) {
 	if ( showClose ) {
 		const closeButton = document.createElement( 'button' );
 		closeButton.textContent = '×';
-		closeButton.onclick = fadeMessage.bind( null, message );
+		closeButton.onclick = function() {
+			message.style.opacity = '0';
+			message.style.transition = 'opacity 0.5s ease-out';
+			setTimeout( () => {
+				message.remove();
+			}, 500 );
+		}
 		message.appendChild( closeButton );
 	}
 
