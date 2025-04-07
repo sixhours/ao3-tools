ca7407c488680379a253cbe2d398fbb33d555909
Caroline Moore <calobee@gmail.com>
2025-04-07 17:44:04 -0400

    Add icon

--- helpers.js
+++ helpers.js
@@ -1,38 +1,38 @@
-function fadeMessage( message ) {
-	setTimeout( () => {
-		if ( message ) {
-			message.style.opacity = '0';
-			message.style.transition = 'opacity 0.5s ease-out';
-			setTimeout( () => {
-				message.remove();
-			}, 500 );
-		}
-	}, 3000 );
-}
-
-function createMessage( content, showClose = true ) {
-	const message = document.createElement( 'div' );
-	message.innerHTML = content;
-	message.className = 'ao3-tools-message';
-	
-	if ( showClose ) {
-		const closeButton = document.createElement( 'button' );
-		closeButton.textContent = '×';
-		closeButton.onclick = function() {
-			message.style.opacity = '0';
-			message.style.transition = 'opacity 0.5s ease-out';
-			setTimeout( () => {
-				message.remove();
-			}, 500 );
-		}
-		message.appendChild( closeButton );
+{
+	function fadeMessage( message ) {
+		setTimeout( () => {
+			if ( message ) {
+				message.style.opacity = '0';
+				message.style.transition = 'opacity 0.5s ease-out';
+				setTimeout( () => {
+					message.remove();
+				}, 500 );
+			}
+		}, 2000 );
 	}
 
-	return message;
-}
+	function createMessage( content, showClose = true ) {
+		const message = document.createElement( 'div' );
+		message.innerHTML = content;
+		message.className = 'ao3-tools-message';
+		if ( showClose ) {
+			const closeButton = document.createElement( 'button' );
+			closeButton.textContent = '×';
+			closeButton.onclick = function() {
+				message.style.opacity = '0';
+				message.style.transition = 'opacity 0.5s ease-out';
+				setTimeout( () => {
+					message.remove();
+				}, 500 );
+			}
+			message.appendChild( closeButton );
+		}
+		return message;
+	}
 
-function createMessageContainer() {
-	const messageContainer = document.createElement( 'div' );
-	messageContainer.id = 'ao3-tools-messages';
-	return messageContainer;
+	function createMessageContainer() {
+		const messageContainer = document.createElement( 'div' );
+		messageContainer.id = 'ao3-tools-messages';
+		return messageContainer;
+	}
 }
